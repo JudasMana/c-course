6d33aeb3ce49ee2cf4d6f1c1c1defc19ff9c4f1b
Davide Manassi <davidemanassi99@gmail.com>
2025-11-03 20:39:36 +0100

    Finished Hello World

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-  printf("Hello world");
+  printf("Select an instruction between add or subtract:\n");
+  char instruction[100];
+  scanf("%s", &instruction);
+
+  int num1;
+  printf("Type first number:\n");
+  scanf("%d", &num1);
+  printf("\n");
+
+  int num2;
+  printf("Type second number:\n");
+  scanf("%d", &num2);
+  printf("\n");
+
+  if (strcmp(instruction, "subtract") == 0){
+    printf("Result: %d\n", num1 - num2);
+  } else if (strcmp(instruction, "add") == 0){
+    printf("Result: %d\n", num1 + num2);
+  }
+
   return 0;
 }
