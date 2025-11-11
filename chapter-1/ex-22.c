e8757cbd9c1f2294efeda8e8ad1c95aaa27e2449
Davide Manassi <davidemanassi99@gmail.com>
2025-11-11 22:00:52 +0100

    1-22 working first part, missing newline adding logic when no space is available

---
+++ chapter-1/ex-22.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#define MAX_LINE_LENGTH 10
+#define MAX_LENGTH 1000
+#define SPACE_NOT_FOUND -1
+
+void foldLines(char text[], int maxLineLength);
+
+int main(){
+  int c, i;
+  char input[MAX_LENGTH];
+
+  i = 0;
+  while(i < MAX_LENGTH - 1 && (c = getchar()) != EOF){
+    input[i] = c;
+    ++i;
+  }
+  ++i;
+  input[i] = '\0';
+  foldLines(input, MAX_LINE_LENGTH);
+
+  printf("\n\nFolded input:\n%s", input);
+  return 0;
+}
+
+void foldLines(char text[], int maxLineLength){
+  printf("Folding lines that exceed characters: %d\n", maxLineLength);
+  int i, lineLength, lastSpaceIndex, lineStartIndex;
+
+  i = lineLength = lineStartIndex = 0;
+  lastSpaceIndex = SPACE_NOT_FOUND;
+
+  while(text[i] != '\0'){
+    if (i == MAX_LENGTH - 1) {
+      text[MAX_LENGTH - 1] = '\0';
+      return;
+    }
+
+    ++lineLength;
+    
+    if (text[i] == ' ' || text[i] == '\t'){
+      lastSpaceIndex = i;
+    } else if (text[i] == '\n'){
+      lineLength = 0;
+      lastSpaceIndex = i;
+      lineStartIndex = i + 1;
+    }
+
+    if (lineLength == maxLineLength){
+      if (lastSpaceIndex == SPACE_NOT_FOUND || (lastSpaceIndex - lineStartIndex) > maxLineLength){
+        printf("Inserting newline on character with index: %d\n", i);
+        text[i] = '\n';  
+        lineLength = i - lineStartIndex;
+        lineStartIndex = lastSpaceIndex + 1;
+      } else {
+        printf("Inserting newline on space with index: %d\n", lastSpaceIndex);
+        text[lastSpaceIndex] = '\n';
+        lineStartIndex = lastSpaceIndex + 1;
+        lineLength = i - lineStartIndex;
+        lastSpaceIndex = SPACE_NOT_FOUND;
+      }
+    }
+
+    ++i;
+  }
+}
