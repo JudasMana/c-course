c567d04481b0947f215c7a0a2c7a0cadd7c0551a
Davide Manassi <davidemanassi99@gmail.com>
2025-11-27 23:30:28 +0100

    Finished 4-14

---
+++ chapter-4/ex-14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define swap(t, x, y) {t temp = x; x = y; y = temp;};
+
+int main(){
+  int a = 1;
+  int b = 2;
+  swap(int, a, b);
+  printf("New a: %d\nNew b: %d", a, b);
+  return 0;
+}
